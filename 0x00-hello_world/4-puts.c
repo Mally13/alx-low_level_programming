890c2dc02c8a0d60aa026800a4b6cbb632712ac6
Mally13 <mallymutuku13@gmail.com>
2023-02-17 07:51:01 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
- *A program to print a line through a function(puts)
- *This program should return 0
+ *main - A program to print a line through a function(puts)
+ *return:0 (Success) - This program should return 0
  *This program shouldn't use printf
  */
 int main(void)
