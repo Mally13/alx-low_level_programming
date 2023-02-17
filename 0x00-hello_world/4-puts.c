c01f94a502ad45f6ecac25480a0f691a4651c67c
Mally13 <mallymutuku13@gmail.com>
2023-02-17 07:14:00 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *A program to print a line through a function(puts)
+ *This program should return 0
+ *This program shouldn't use printf
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
