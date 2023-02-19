2a46a071d907097a85884ced1e6e188d60ffbdaa
Mally13 <mallymutuku13@gmail.com>
2023-02-19 11:50:26 -0800

    stdout

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+include <unistd.h>
+/*
+ * main - a program to print a a line in the standard error
+ * return: 1(Indicates success)
+ */
+int main(void){
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
