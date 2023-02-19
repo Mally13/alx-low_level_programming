5f3d7e35e81ee953be75d7a221dbf85334e62c7c
Mally13 <mallymutuku13@gmail.com>
2023-02-19 12:09:57 -0800

    fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program to print a a line in the standard error
- * return: 1(Indicates success)
+ * main - A program to print a line in the standard error
+ * Return: 1(success)
  */
-int main(void){
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
