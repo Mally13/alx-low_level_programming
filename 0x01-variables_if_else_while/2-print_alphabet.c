d92cc0fcde7deca87cb1706bb407d05c76f8bb3f
Mally13 <mallymutuku13@gmail.com>
2023-02-20 13:34:51 -0800

    for_loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the alphabet in lowercase
+ * Return: 0(succes)
+ */
+
+int main(void)
+{
+	for(i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
