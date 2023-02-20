0cd5c3b9114ef820d7ee0b159888d8703a4da353
Mally13 <mallymutuku13@gmail.com>
2023-02-20 15:23:19 -0800

    hhh

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 	for (i =0; i < 10; i++)
 	{
 		putchar( i + '0');
-		putchar(' ');
 		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
