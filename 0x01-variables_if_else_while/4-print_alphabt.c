4390ccaa99b5bf71051d01797a1110b641821d68
Mally13 <mallymutuku13@gmail.com>
2023-02-20 14:01:29 -0800

    for_loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- prints all the letters except q and e
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+		if ( i != 'e' && i != 'q')
+			putchar(i);
+
+	putchar('\n');
+	return (0);
+}
