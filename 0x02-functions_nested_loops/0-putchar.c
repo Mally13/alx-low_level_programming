92fee695718b1f2dbbeee4b0172de5e2f5135462
Mally13 <mallymutuku13@gmail.com>
2023-02-21 04:55:12 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	char str[8] = "_putchar";
+
+	printf("%s\n", str);
+	return (0);	
+}
