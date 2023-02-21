b555c098b3d034a9f240e65269d129c17ab5cb51
Mally13 <mallymutuku13@gmail.com>
2023-02-20 17:11:07 -0800

    final

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,29 +7,30 @@
 
 int main(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 	int k;
-
-        for (i = 0; i < 10; i++)
-        {
-                for (j = 1; j < 10; j++)
-                {
-			for (k = 2; k < 10; k++){
-				 if (i < j && j < k)
-				 {
-                                	putchar(i + '0');
-                                	putchar(j + '0');
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			for (k = 2; k < 10; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
 					putchar(k + '0');
-                                	if (i + j + k != 24)
+					if (i + j + k != 24)
 					{
 						putchar(',');
-                                       		putchar(' ');
+						putchar(' ');
 					}
-				 }
+				}
 			}
 		}
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
