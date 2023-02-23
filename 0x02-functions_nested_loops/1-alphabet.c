b5bf44445578c64d3e39f009ade802041080afed
Mally13 <mallymutuku13@gmail.com>
2023-02-23 00:43:08 -0800

    1_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet in lowercase
+ *print_alphabet - prints the alphabet in lowercase
  * Return:0
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char i;
-
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	print_alphabet();
-	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *print_alphabet - prints alphabet
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
