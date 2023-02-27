30a5745efe870e69d966a26bd62250ed4b181c19
Mally13 <mallymutuku13@gmail.com>
2023-02-27 02:59:30 -0800

    betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-	int i,j;
+	int i, j;
 
-	for(i = 0; i <= 98; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for(j = i + 1; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
