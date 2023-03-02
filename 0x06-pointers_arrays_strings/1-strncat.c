574ee8ff9fabf1a912bb3b558a22d94a3b91707a
Mally13 <mallymutuku13@gmail.com>
2023-03-02 10:30:18 -0800

    strcat_n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concatenates two strings
+ * it will use at most n bytes from src; and
+ * src does not need to be null-terminated if it contains n
+ * @dest: The string to be merged with n characters from src
+ * @src: String contining n characters or more
+ * @n: Number of characters to be added to dest
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destln = strlen(dest);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[destln + i] = src[i];
+	}
+	return (dest);
+}
