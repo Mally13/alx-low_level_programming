d145d0863b46898989bb011bcefa6537afa358c6
Mally13 <mallymutuku13@gmail.com>
2023-03-06 03:50:35 -0800

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,19 @@
 
 char *leet(char *str)
 {
-	int i;
+	int i = 0;
+	int j;
+	char letters[8] = {'O', 'L', '*', 'E', 'A', '*', '*', 'T'};
 
-	for (i = 0; str[i] && str[i] != '\0'; i++)
+	while (str[i])
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == letters[j] ||
+					str[i] - 32 == letters[j])
+				str[i] = j + '0';
+		}
+		i++;
 	}
 	return (str);
 }
