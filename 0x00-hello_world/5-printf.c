3fcbf7c939f9907e534a6a509b0bc1ffabbffa0e
Mally13 <mallymutuku13@gmail.com>
2023-05-18 07:02:02 -0700

    more c tasks and scripts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- prints a line followed by a new line
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
