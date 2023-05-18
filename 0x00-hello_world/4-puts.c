5f2e8d72512ea8f92ddaacddfa88bbcd66e472a2
Mally13 <mallymutuku13@gmail.com>
2023-05-18 06:17:06 -0700

    correction- betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * C program that prints exactly
  *  "Programming is like building a multilingual puzzle", followed by a new line.
  */
-int main()
+int main(void)
 {
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
