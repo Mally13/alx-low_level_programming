0959c5616bd92f8825e1b017d4a187f460d15cb5
Mally13 <mallymutuku13@gmail.com>
2023-05-18 06:08:23 -0700

    first five tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * C program that prints exactly
+ *  "Programming is like building a multilingual puzzle", followed by a new line.
+ */
+int main()
+{
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myStr);
+	return (0);
+}
+
