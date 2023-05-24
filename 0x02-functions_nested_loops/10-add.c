91bdb012819250e1ef102d99f14790af82e0dd49
Mally13 <mallymutuku13@gmail.com>
2023-05-24 04:17:38 -0700

    more functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: first number
+ * @b: second interger
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number to start from
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else
+	{
+		for (; n >= 97; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+}
