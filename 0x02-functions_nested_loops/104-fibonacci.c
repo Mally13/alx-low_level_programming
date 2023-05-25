fd1c2f8f39ab4f3e0005817a0c46cb0cf80ee9d8
Mally13 <mallymutuku13@gmail.com>
2023-05-25 09:29:51 -0700

    Update readme

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,16 +5,38 @@
  */
 int main(void)
 {
-	unsigned long i, j = 1, k = 2;
+	unsigned long i, j = 1, k = 2, n1, n2, n1half1, n1half2, n2half1, n2half2;
 
 	for (i = 0; i < 49; i++)
 	{
-		if (i != 48)
+		if (i < 46)
+		{
 			printf("%lu, %lu, ", j, k);
+			j += k;
+			k += j;
+		}
 		else
-			printf("%lu, %lu", j, k);
-		j += k;
-		k += j;
+		{
+			n1half1 = j / 10000000000;
+			n1half2 = j % 10000000000;
+			n2half1 = k / 10000000000;
+			n2half2 = k % 10000000000;
+			n1 = n1half1 + n2half1;
+		       	n2 = n1half2 + n2half2;
+			if (n1half2 + n2half2 > 9999999999)
+			{
+				n1 += 1;
+				n2 %= 10000000000;
+			}
+			if (i != 48)
+				printf("%lu, %lu, ", n1, n2);
+			else
+				printf("%lu, %lu", n1, n2);
+			n1half1 = n2half1;
+			n1half2 = n2half2;
+			n2half1 = n1;
+			n2half2 = n2;
+		}
 	}
 	printf("\n");
 	return (0);
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-#Low Level Programming Projects
+# Low Level Programming Projects
 
 1. Hello World - 0x00-hello_world
 2. Variables, if ... else, while - 0x01-variables_if_else_while
 3. Functions, nested loops - 0x02-functions_nested_loops
+4. Debugging - 0x03-debugging
+5. More functions and nested loops - 0x04-more_functions_nested_loops
