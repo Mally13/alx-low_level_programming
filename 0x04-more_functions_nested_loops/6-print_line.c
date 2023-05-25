d1e8fa89c4c22a3f09160fb40e57c39d06a06807
Mally13 <mallymutuku13@gmail.com>
2023-05-26 01:02:31 +0300

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line -draws a straight line in the terminal
+ * @n: length of the line
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
