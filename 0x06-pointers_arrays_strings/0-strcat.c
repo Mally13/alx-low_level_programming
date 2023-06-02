357e088f3a8b313bf9a0f1fa590342c8c5470c44
Mally13 <mallymutuku13@gmail.com>
2023-06-02 10:05:16 +0300

    Task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates teo strings
+ * @dest: pointer to the first string
+ * @src: pointer to the second string to be appended
+ * Return: pointer to the final string after concatenation
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lndest = 0 , lnsrc = 0, i;
+
+	while (*dest != '\0')
+	{
+		lndest++;
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		lnsrc++;
+		src++;
+	}
+
+	for (i = 0; i <= lnsrc; i++)
+	{
+		dest[lndest + i] = src[i];
+	}
+	return (dest);
+
+}
