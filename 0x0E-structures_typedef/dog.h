c30d2cbcff9cb96ff3dc6dfcd0718919a0c7a233
Mally13 <mallymutuku13@gmail.com>
2023-06-20 12:08:54 +0300

    Structure dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/* A header file that defines type struct dog */
+
+/**
+ * struct dog - A type describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif/* endif ifndef DOG_H */
