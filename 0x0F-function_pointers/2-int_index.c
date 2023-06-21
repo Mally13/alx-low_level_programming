cdad0a64077ed835999f64d0d6591da83289a1eb
Mally13 <mallymutuku13@gmail.com>
2023-06-21 14:49:12 +0300

    Function Pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,10 +16,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
-		if (cmp(array[i] != 0))
-				return (i);
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 	return (-1);
 }
