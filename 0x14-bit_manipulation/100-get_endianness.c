21d821735cb9722851fc49a1a2541425294f05d5
Mally13 <mallymutuku13@gmail.com>
2023-07-25 22:02:21 +0300

    Endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endiannes - checks the endianness
+ * get_endianness- checks the endianness
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
