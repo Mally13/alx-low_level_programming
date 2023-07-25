a21a2ab0701ddfaece533e4c6cca93de8d806aa5
Mally13 <mallymutuku13@gmail.com>
2023-07-25 15:14:58 +0300

    Fix: Remove % and /

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,18 +5,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int rem = 0;
-
-	if (n == 0)
-		_putchar(rem + '0');
-	else
+	if (n > 1)
 	{
-		while (n > 0)
-		{
-
-			rem = n % 2;
-			_putchar(rem + '0');
-			n /= 2;
-		}
+		print_binary(n >> 1);
 	}
+	_putchar((n & 1) + '0');
+
 }
