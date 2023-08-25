7273c671f3a15e1d87d413af3b230ee13e0a6221
Mally13 <mallymutuku13@gmail.com>
2023-08-25 17:57:19 +0300

    Task 100

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - gets the difference betwwen two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: Difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - gets the product of two intergers
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - gets the quotient of two intergers
+ * @a:first int
+ * @b: second int
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - gets the modulus of two integers
+ * @a: first int
+ * @b: second int
+ * Return: modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
